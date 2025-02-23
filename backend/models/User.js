c53c424999b7e483d62d522b8c0c9bfcbcf24a29
BrentMRPalmer <bpalm015@uottawa.ca>
2025-02-22 21:08:50 -0500

    Registration endpoint v1

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,12 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 // Define User schema
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }
-});
+}, { timestamps: true }); // Adds `createdAt` and `updatedAt`
 
 // Export the User model
-module.exports = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
+export default User;
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,21 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
+import authRoutes from "./routes/auth.js"
 
 // Creates an Express server instance
 const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
 
 // Set environment variables
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 // Connect to MongoDB
-mongoose.connect(MONGO_URI)
+mongoose.connect(MONGO_URI, {dbName: "areto"})
     .then(() => {
         console.log("MongoDB connected")
         app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
@@ -19,7 +25,8 @@ mongoose.connect(MONGO_URI)
         process.exit(1);
     })
 
-
+// Routes
+app.use("/api", authRoutes); // Register the auth routes
 
 
 
