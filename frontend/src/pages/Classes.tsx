5a3ef22d073eeb84a555f5377f89fe1ebef45ec7
Noah do Rego <ndore051@uottawa.ca>
2025-03-07 20:53:19 -0500

    Added basic layout for classes page

--- frontend/src/pages/Classes.tsx
+++ frontend/src/pages/Classes.tsx
@@ -1,9 +1,57 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 const Classes = () => {
-    return (
-      <div className="px-32 mt-12">
-        <h1 className="text-5xl font-bold">My Classes</h1>
+  const dummyCourses = [
+    {
+      code: "CSI2132",
+      institution: "uOttawa",
+      name: "Databases",
+    },
+    {
+      code: "CSI2132",
+      institution: "uOttawa",
+      name: "Databases",
+    },
+    {
+      code: "CSI2132",
+      institution: "uOttawa",
+      name: "Databases",
+    },
+  ];
+  return (
+    <div className="px-32 mt-12">
+      <h1 className="text-5xl font-bold mb-6">My Classes</h1>
+      <div className="flex flex-row justify-between mb-8">
+        <h3>
+          <b>Term:</b> Winter 2025
+        </h3>
+        <h3>[Search Placeholder]</h3>
       </div>
-    );
-  };
-  
-  export default Classes;
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 max-w-6xl w-full mx-auto">
+        {dummyCourses.map((course) => (
+          <Card>
+            <CardHeader>
+              <CardTitle>{course.code}</CardTitle>
+              <CardDescription>{course.name}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Classes;
